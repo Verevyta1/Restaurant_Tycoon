513cf672535791672e88e0cb01cda3d45b7b1532
Marius Uliu <78550987+Verevyta1@users.noreply.github.com>
2026-08-16 22:58:48 +0100

    Clear project for fresh start

--- game.js
+++
@@ -1,6 +0,0 @@
-const startButton = document.querySelector('#start-game');
-const gameMessage = document.querySelector('#game-message');
-
-startButton.addEventListener('click', () => {
-  gameMessage.textContent = 'Your restaurant is ready for its first idea.';
-});
